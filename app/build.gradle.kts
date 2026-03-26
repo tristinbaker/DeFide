1b757ace7d0211ae0f3c50a11292e538f347763a
Tristin Baker <tristinbaker52@gmail.com>
2026-03-26 07:48:44 -0400

    Bump version to 1.1.1 (versionCode 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.tristinbaker.defide"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
